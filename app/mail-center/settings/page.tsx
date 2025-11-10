abf50b4daee4d94fa001b8a243e9ea1cc9d4a897
Laszlo Jean-Pierre <laszlojeanpierre@gmail.com>
2025-11-10 15:30:57 +0100

    chore: Force rebuild by adding comment

--- app/mail-center/settings/page.tsx
+++ app/mail-center/settings/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+// Page de paramètres utilisateur avec gestion d'abonnement
+
 import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
